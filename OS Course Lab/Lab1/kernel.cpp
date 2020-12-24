24992fe4867bed0b0783c9d0a431fcbd0c4873cc
JeffFu <1093928135@qq.com>
2020-12-24 14:05:25 +0800

    timeout & list

--- OS Course Lab/Lab1/kernel.cpp
+++ OS Course Lab/Lab1/kernel.cpp
@@ -5,46 +5,58 @@
 #include "kernel.h"
 #include "iostream"
 #include "string"
+#include "list"
 #include "PCB.h"
 using namespace std;
 
-//bool proc_init = false;
 // res: 0, 1, 2, 3
 int res_remain[4] = {1, 1, 2, 2};
 // priority 0, 1, 2: init, user, system
-vector<PCB*> ready_list[3];
-vector<PCB*> block_list[3];
-PCB* running_proc;
+list<PCB*> ready_list[3];
+list<PCB*> block_list[3];
+// NULL if not initialized
+PCB* running_proc = NULL;
 
 void scheduler() {
+    // choose the correct process from ready list and set the status to running
+    // make sure init is in the ready list
     for (int i = 2; i >= 0; i--) {
         if (ready_list[i].empty()) continue;
-        else {
-            cout << "Process " << ready_list[i].front()->get_pid() << " is running" << endl;
-            break;
-        }
+        // else
+        running_proc = ready_list[i].front();
+        ready_list[i].pop_front();
+        running_proc->set_status("running");
+        break;
     }
+    cout << "Process " << running_proc->get_pid() << " is running." << endl;
 }
 
 void init() {
+    if (running_proc != NULL) {
+        cout << "Already initialized." << endl;
+        cout << "Process " << running_proc->get_pid() << " is running." << endl;
+        return;
+    }
     PCB* p = new PCB("init", 0);
     ready_list[0].push_back(p);
     scheduler();
 }
 
 void create_process(string name, int priority) {
-    if (ready_list[0].empty()) {
+    if (running_proc == NULL) {
         cout << "Please run init process at first." << endl;
         return;
     }
     // create process 'name', priority 'priority'
     PCB* p = new PCB(name, priority);
     ready_list[priority].push_back(p);
+    running_proc->set_status("ready");
+    ready_list[running_proc->get_priority()].push_front(running_proc);
     scheduler();
 }
 
 void delete_process(string name) {
-    if (ready_list[0].empty()) {
+    if (running_proc == NULL) {
         cout << "Please run init process at first." << endl;
         return;
     }
@@ -52,7 +64,7 @@ void delete_process(string name) {
 }
 
 void request_res(string name, int id) {
-    if (ready_list[0].empty()) {
+    if (running_proc == NULL) {
         cout << "Please run init process at first." << endl;
         return;
     }
@@ -60,7 +72,7 @@ void request_res(string name, int id) {
 }
 
 void release_res(string name, int id) {
-    if (ready_list[0].empty()) {
+    if (running_proc == NULL) {
         cout << "Please run init process at first." << endl;
         return;
     }
@@ -68,19 +80,19 @@ void release_res(string name, int id) {
 }
 
 void time_out() {
-    if (ready_list[0].empty()) {
+    if (running_proc == NULL) {
         cout << "Please run init process at first." << endl;
         return;
     }
-    cout << "System timeout interrupt" << endl;
+    running_proc->set_status("ready");
+    ready_list[running_proc->get_priority()].push_back(running_proc);
+    scheduler();
 }
 
 void list_info(string option) {
-//    if (!proc_init) {
-//        cout << "Please run init process at first." << endl;
-//        return;
-//    }
+    // number of valid options
     const int num_valid_op = 3;
+    // valid options
     string valid_ops[num_valid_op] = {"ready", "block", "res"};
     // get option id
     int op_id;
@@ -90,10 +102,31 @@ void list_info(string option) {
     // execute command with option
     switch (op_id) {
         // valid options
-        // todo: ready
-        case 0: cout << "list ready" << endl; break;
-        // todo: block
-        case 1: cout << "list block" << endl; break;
+        // list ready
+        case 0: {
+            for (int i = 2; i >= 0; i--) {
+                cout << i << ": ";
+                for (PCB* tmp : ready_list[i]) {
+                    cout << tmp->get_pid();
+                    if (tmp != ready_list[i].back()) cout << "-";
+                }
+                cout << endl;
+            }
+            break;
+        }
+        // list block
+        case 1: {
+            for (int i = 2; i >= 0; i--) {
+                cout << i << ": ";
+                for (PCB* tmp : block_list[i]) {
+                    cout << tmp->get_pid();
+                    if (tmp != block_list[i].back()) cout << "-";
+                }
+                cout << endl;
+            }
+            break;
+        }
+        // list res
         case 2: {
             for (int i = 0; i < 4; i++) {
                 cout << "R" << i+1 << " " << res_remain[i] << endl;
